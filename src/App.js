0ff3a411838ae9be8b8580b0c48ff7814bf441a6
pramesh101 <prameshsingh2411@gmail.com>
2023-04-02 22:16:03 +0530

    Calculator Web

--- src/App.js
+++ src/App.js
@@ -1,23 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState, useEffect } from "react";
+import BarLoader from "react-spinners/BarLoader";
 function App() {
+  const [loading, setLoading] = useState(false)
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+
+    }, 5000)
+  }, [])
+
+  const [calc, setCalc] = useState("");
+  const [result, setResult] = useState("");
+
+  const ops = ['/', '*', '+', '-', '.'];
+
+  const updateCalc = value => {
+    if (
+      (ops.includes(value)) && (calc === '' ||             //this code used for No double operators
+        ops.includes(value)) && (ops.includes(calc.slice(-1)//only one operator one time 
+        ))
+    ) {
+      return;
+    }
+    setCalc(calc + value);
+
+    if (!ops.includes(value)) {
+      setResult(eval(calc + value).toString());
+    }
+  }
+
+  const createDigits = () => {
+    const digits = [];
+    for (let i = 1; i < 10; i++) {
+      digits.push(
+        <button onClick={() => updateCalc(i.toString())}
+          key={i}>
+          {i}
+        </button>
+      )
+    }
+
+    return digits;
+  }
+
+  const calculate = () => {
+    setCalc(eval(calc).toString());
+  }
+  const deleteLast = () => {
+    if (calc === '') {
+      return;
+    }
+    const value = calc.slice(0, -1);
+
+    setCalc(value);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+
+        loading ?
+          <BarLoader
+            color={"#d73797"}
+            loading={loading}
+            size={200}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+          :
+          <div className="Calculator">
+            <div className="display">
+              {result ? <span>({result})</span> : ''} &nbsp;
+              {calc || "0"}
+            </div>
+            <div className="operators">
+              <button onClick={() => updateCalc('/')}>/</button>
+              <button onClick={() => updateCalc('*')}>*</button>
+              <button onClick={() => updateCalc('+')}>+</button>
+              <button onClick={() => updateCalc('-')}>-</button>
+              <button onClick={deleteLast}>DEL</button>
+            </div>
+            <div className="digits">
+              {createDigits()}
+              <button onClick={() => updateCalc('0')}>0</button>
+              <button onClick={() => updateCalc('.')}>.</button>
+
+              <button onClick={calculate}>=</button>
+            </div>
+          </div>
+      }
     </div>
   );
 }
